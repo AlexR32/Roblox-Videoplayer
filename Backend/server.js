f6481d6aff16ddbf680a77f258271a13450da181
AlexR32 <58864658+AlexR32@users.noreply.github.com>
2021-10-01 18:14:55 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -14,33 +14,37 @@ app.post('/youtube', async function(request, responce) {
     let videoId = 'https://youtu.be/' + body.videoId
     let validURL = ytdl.validateURL(videoId)
     if (validURL) {
-        console.log('downloading ' + videoId)
-        let video = ytdl(videoId, {quality: 247}) // only 720p webm
-        let audio = ytdl(videoId, {quality: 'highestaudio'})
         let info = await ytdl.getInfo(videoId)
-        const ffmpegProcess = childProcess.spawn(ffmpeg, [
-            '-loglevel', '8', '-hide_banner',
-            '-i', 'pipe:3',
-            '-i', 'pipe:4',
-            '-map', '0:v',
-            '-map', '1:a',
-            '-metadata','duration=' + info.videoDetails.lengthSeconds,
-            '-c:v', 'copy',
-            '-f', "webm",
-            'pipe:5',
-        ], {
-            windowsHide: true,
-            stdio: [
-                'inherit', 'inherit', 'inherit',
-                'pipe', 'pipe', 'pipe',
-            ],
-        })
-        video.pipe(ffmpegProcess.stdio[3])
-        audio.pipe(ffmpegProcess.stdio[4])
-        ffmpegProcess.stdio[5].pipe(responce)
-        ffmpegProcess.on('close', () => {
-            console.log('downloaded ' + videoId)
-        })
+        if (info.quality === '720p') {
+            console.log('downloading ' + videoId)
+            let video = ytdl(videoId, {quality: 247}) // only 720p webm
+            let audio = ytdl(videoId, {quality: 'highestaudio'})
+            const ffmpegProcess = childProcess.spawn(ffmpeg, [
+                '-loglevel', '8', '-hide_banner',
+                '-i', 'pipe:3',
+                '-i', 'pipe:4',
+                '-map', '0:v',
+                '-map', '1:a',
+                '-metadata','duration=' + info.videoDetails.lengthSeconds,
+                '-c:v', 'copy',
+                '-f', "webm",
+                'pipe:5',
+            ], {
+                windowsHide: true,
+                stdio: [
+                    'inherit', 'inherit', 'inherit',
+                    'pipe', 'pipe', 'pipe',
+                ],
+            })
+            video.pipe(ffmpegProcess.stdio[3])
+            audio.pipe(ffmpegProcess.stdio[4])
+            ffmpegProcess.stdio[5].pipe(responce)
+            ffmpegProcess.on('close', () => {
+                console.log('downloaded ' + videoId)
+            })
+        } else {
+            responce.sendStatus(404)
+        }
     } else {
         responce.sendStatus(404)
     }
