0afe7258248ca074b458df1ecc99f7c66b67b696
AlexR32 <58864658+AlexR32@users.noreply.github.com>
2021-10-01 19:22:15 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,6 @@ const childProcess = require('child_process')
 const ffmpeg = require('ffmpeg-static')
 const ytdl = require('ytdl-core')
 const express = require('express')
-const { format } = require('path')
 const app = express()
 const port = process.env.PORT || 80
 
@@ -25,35 +24,35 @@ app.post('/youtube', async function(request, responce) {
     if (validURL) {
         let info = await ytdl.getInfo(videoId)
         if (checkItag(info)) {
-        console.log('downloading ' + videoId)
-        let video = ytdl(videoId, {quality: 247}) // only 720p webm
-        let audio = ytdl(videoId, {quality: 'highestaudio'})
-        const ffmpegProcess = childProcess.spawn(ffmpeg, [
-            '-loglevel', '8', '-hide_banner',
-            '-i', 'pipe:3',
-            '-i', 'pipe:4',
-            '-map', '0:v',
-            '-map', '1:a',
-            '-metadata','duration=' + info.videoDetails.lengthSeconds,
-            '-c:v', 'copy',
-            '-f', "webm",
-            'pipe:5',
-        ], {
-            windowsHide: true,
-            stdio: [
-                'inherit', 'inherit', 'inherit',
-                'pipe', 'pipe', 'pipe',
-            ],
-        })
-        video.pipe(ffmpegProcess.stdio[3])
-        audio.pipe(ffmpegProcess.stdio[4])
-        ffmpegProcess.stdio[5].pipe(responce)
-        ffmpegProcess.on('close', () => {
-            console.log('downloaded ' + videoId)
-        })
-    } else {
-        responce.sendStatus(404)
-    }
+            console.log('downloading ' + videoId)
+            let video = ytdl(videoId, {quality: 247}) // only 720p webm
+            let audio = ytdl(videoId, {quality: 'highestaudio'})
+            const ffmpegProcess = childProcess.spawn(ffmpeg, [
+                '-loglevel', '8', '-hide_banner',
+                '-i', 'pipe:3',
+                '-i', 'pipe:4',
+                '-map', '0:v',
+                '-map', '1:a',
+                '-metadata','duration=' + info.videoDetails.lengthSeconds,
+                '-c:v', 'copy',
+                '-f', "webm",
+                'pipe:5',
+            ], {
+                windowsHide: true,
+                stdio: [
+                    'inherit', 'inherit', 'inherit',
+                    'pipe', 'pipe', 'pipe',
+                ],
+            })
+            video.pipe(ffmpegProcess.stdio[3])
+            audio.pipe(ffmpegProcess.stdio[4])
+            ffmpegProcess.stdio[5].pipe(responce)
+            ffmpegProcess.on('close', () => {
+                console.log('downloaded ' + videoId)
+            })
+        } else {
+            responce.sendStatus(404)
+        }
     } else {
         responce.sendStatus(404)
     }
