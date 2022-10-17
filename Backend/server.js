9a96cad2d9128672d9f4b6af54ad6105b9da9790
AlexR32 <58864658+AlexR32@users.noreply.github.com>
2022-10-17 19:20:54 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,63 +1,58 @@
 const childProcess = require('child_process')
 const ffmpeg = require('ffmpeg-static')
-const ytdl = require('ytdl-core')
 const express = require('express')
+const ytdl = require('ytdl-core')
+
 const app = express()
+app.use(express.json())
 const port = process.env.PORT || 80
 
-app.use(express.static('static'))
-app.use(express.json())
-
-function checkItag(info) {
+function isCorrectItag(info, itag) {
     for (i in info.formats) {
-        if (info.formats[i].itag === 247) {
+        if (info.formats[i].itag === itag) {
             return true
         }
     }
 }
 
-app.post('/youtube', async function(request, responce) {
-    if (!request.body && !request.body.videoId) return responce.sendStatus(404)
-    let body = request.body
-    let videoId = 'https://youtu.be/' + body.videoId
-    let validURL = ytdl.validateURL(videoId)
-    if (validURL) {
-        let info = await ytdl.getInfo(videoId)
-        if (checkItag(info)) {
-            console.log('downloading ' + videoId)
-            let video = ytdl(videoId, {quality: 247}) // only 720p webm
-            let audio = ytdl(videoId, {quality: 'highestaudio'})
-            const ffmpegProcess = childProcess.spawn(ffmpeg, [
+app.post('/yt/video', async (req, res) => {
+    if (!req.query.videoId) return res.sendStatus(404)
+    let videoURL = 'https://youtu.be/' + req.query.videoId
+    let validURL = ytdl.validateURL(videoURL)
+
+    if (validURL) { let info = await ytdl.getInfo(videoURL)
+        if (isCorrectItag(info, 247)) {
+            let video = ytdl.downloadFromInfo(info, { quality: 247 })
+            let audio = ytdl.downloadFromInfo(info, { quality: 'highestaudio' })
+            let ffmpegProcess = childProcess.spawn(ffmpeg, [
                 '-loglevel', '8', '-hide_banner',
-                '-i', 'pipe:3',
-                '-i', 'pipe:4',
-                '-map', '0:v',
-                '-map', '1:a',
+                '-i', 'pipe:0', '-i', 'pipe:1', '-map', '0:v', '-map', '1:a',
                 '-metadata','duration=' + info.videoDetails.lengthSeconds,
-                '-c:v', 'copy',
-                '-f', "webm",
-                'pipe:5',
-            ], {
-                windowsHide: true,
-                stdio: [
-                    'inherit', 'inherit', 'inherit',
-                    'pipe', 'pipe', 'pipe',
-                ],
-            })
-            video.pipe(ffmpegProcess.stdio[3])
-            audio.pipe(ffmpegProcess.stdio[4])
-            ffmpegProcess.stdio[5].pipe(responce)
-            ffmpegProcess.on('close', () => {
-                console.log('downloaded ' + videoId)
-            })
-        } else {
-            responce.sendStatus(404)
-        }
-    } else {
-        responce.sendStatus(404)
-    }
+                '-c:v', 'copy', '-f', 'webm', 'pipe:2'
+            ])
+            video.pipe(ffmpegProcess.stdio[0])
+            audio.pipe(ffmpegProcess.stdio[1])
+            ffmpegProcess.stdio[2].pipe(res)
+        } else { res.sendStatus(404) }
+    } else { res.sendStatus(404) }
+})
+
+app.post('/yt/audio', async (req, res) => {
+    if (!req.query.videoId) return res.sendStatus(404)
+    let videoURL = 'https://youtu.be/' + req.query.videoId
+    let validURL = ytdl.validateURL(videoURL)
+
+    if (validURL) { let info = await ytdl.getInfo(videoURL)
+        let audio = ytdl.downloadFromInfo(info, { quality: 'highestaudio' })
+        let ffmpegProcess = childProcess.spawn(ffmpeg, [
+            '-loglevel', '8', '-hide_banner',
+            '-i', 'pipe:0', '-f', 'mp3', 'pipe:1'
+        ])
+        audio.pipe(ffmpegProcess.stdio[0])
+        ffmpegProcess.stdio[1].pipe(res)
+    } else { res.sendStatus(404) }
 })
 
 app.listen(port, () => {
-    console.log('server running')
+    console.log(`${new Date().toLocaleString()} | server running on port ${port}`)
 })
