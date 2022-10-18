2aaa340d09a0a69f64d12d90e71ecc62f26628aa
AlexR32 <58864658+AlexR32@users.noreply.github.com>
2022-10-18 19:29:08 +0300

    Update Audioplayer.lua

--- Audioplayer.lua
+++ Audioplayer.lua
@@ -30,10 +30,11 @@ local Title = Window.Title
 local Playback = Window.Playback
 local ControlPanel = Window.ControlPanel
 
+local VSActive,PSActive = false,false
 local Domain = "https://parvus.fun/"
 if not isfolder("audios") then
 	makefolder("audios")
-end
+end local Playing = false
 
 local function MakeDraggable(Dragger,Object,Callback)
 	local StartPosition,StartDrag = nil,nil
@@ -93,8 +94,17 @@ local function UpdatePlayback()
 	Playback.Line.Size = UDim2.new(Audio.TimePosition / Audio.TimeLength,0,1,0)
 end
 
-local function ChangePlayback()
-	local Position = math.clamp((UserInputService:GetMouseLocation().X - Playback.AbsolutePosition.X) / Playback.AbsoluteSize.X,0,1)
+local function UpdateVS(Input)
+	local XScale = math.clamp((Input.Position.X - ControlPanel.VolSlider.AbsolutePosition.X) / ControlPanel.VolSlider.AbsoluteSize.X,0,1)
+	local SliderPrecise = math.round(math.clamp(XScale * (200 - 0) + 0,0,200))
+
+	ControlPanel.VolSlider.Title.Text = tostring(SliderPrecise) .. "%"
+	ControlPanel.VolSlider.Line.Size = UDim2.new((SliderPrecise - 0) / (200 - 0),0,1,0)
+	Audio.Volume = SliderPrecise / 100
+end
+
+local function UpdatePS(Input)
+	local Position = math.clamp((Input.Position.X - Playback.AbsolutePosition.X) / Playback.AbsoluteSize.X,0,1)
 	Audio.TimePosition = math.clamp(Audio.TimeLength * Position,0,Audio.TimeLength) UpdateTime() UpdatePlayback()
 end
 
@@ -120,34 +130,67 @@ end
 
 local function LoadAudio(Enter)
 	if Enter then
-		local VideoId = ControlPanel.LinkInput.Text
-		ControlPanel.LinkInput.PlaceholderText = "Video ID (Loading)"
-		ControlPanel.LinkInput.Text = ""
+		local VideoId = ControlPanel.VIDInput.Text
+		ControlPanel.VIDInput.PlaceholderText = "Video ID (Loading)"
+		ControlPanel.VIDInput.Text = ""
 
 		if not isfile("audios/" .. VideoId .. ".mp3") then
 			local Video = RequestVideo(VideoId) if Video then
 				writefile("audios/" .. VideoId .. ".mp3", Video)
 				Audio.SoundId = GetAsset("audios/" .. VideoId .. ".mp3")
 				Title.Text = "Audio Player - audios/" .. VideoId .. ".mp3"
-				ControlPanel.LinkInput.PlaceholderText = "Video ID"
-				AudioMode("Play")
+				ControlPanel.VIDInput.PlaceholderText = "Video ID"
+				AudioMode("Play") Playing = Audio.Playing
 			else
-				ControlPanel.LinkInput.PlaceholderText = "Video ID (Failed)"
-				Window.Title.Text = "Audio Player" AudioMode("Stop")
+				ControlPanel.VIDInput.PlaceholderText = "Video ID (Failed)"
+				Window.Title.Text = "Audio Player"
+				AudioMode("Stop") Playing = Audio.Playing
 			end
 		else
-			ControlPanel.LinkInput.PlaceholderText = "Video ID"
+			ControlPanel.VIDInput.PlaceholderText = "Video ID"
 			Audio.SoundId = GetAsset("audios/" .. VideoId .. ".mp3")
 			Window.Title.Text = "Audio Player - audios/" .. VideoId .. ".mp3"
-			AudioMode("Play")
+			AudioMode("Play") Playing = Audio.Playing
 		end
 	end
 end
 
-Window.Playback.Slider.MouseButton1Click:Connect(ChangePlayback)
-ControlPanel.LinkInput.FocusLost:Connect(LoadAudio)
+ControlPanel.VolSlider.InputBegan:Connect(function(Input)
+	if Input.UserInputType == Enum.UserInputType.MouseButton1 then
+		UpdateVS(Input) VSActive = true
+	end
+end)
+ControlPanel.VolSlider.InputEnded:Connect(function(Input)
+	if Input.UserInputType == Enum.UserInputType.MouseButton1 then
+		VSActive = false
+	end
+end)
+
+Playback.Slider.InputBegan:Connect(function(Input)
+	if Input.UserInputType == Enum.UserInputType.MouseButton1 then
+		AudioMode("Pause") UpdatePS(Input) PSActive = true
+	end
+end)
+
+Playback.Slider.InputEnded:Connect(function(Input)
+	if Input.UserInputType == Enum.UserInputType.MouseButton1 then
+		if Playing then AudioMode("Resume") end
+		PSActive = false
+	end
+end)
+
+UserInputService.InputChanged:Connect(function(Input)
+	if Input.UserInputType == Enum.UserInputType.MouseMovement then
+		if VSActive then UpdateVS(Input) end
+		if PSActive then UpdatePS(Input) end
+	end
+end)
+
+
+ControlPanel.VIDInput.FocusLost:Connect(LoadAudio)
 ControlPanel.Play.MouseButton1Click:Connect(function()
 	AudioMode(not Audio.Playing and "Resume" or "Pause")
+	Playing = Audio.Playing
 end) ControlPanel.Repeat.MouseButton1Click:Connect(function()
 	AudioMode(Audio.Looped and "LoopOff" or "LoopOn")
 end) RunService.RenderStepped:Connect(function()
